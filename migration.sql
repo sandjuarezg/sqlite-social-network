df48498fd0a3b5df0a517c7311c150e9ccae841c
Sandra Juarez <sandjuarezg@gmail.com>
2021-12-23 20:13:18 -0600

    chore: database structure

--- migration.sql
+++ migration.sql
@@ -1,7 +1,8 @@
 -- creates
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    username TEXT NOT NULL,   
     passwd TEXT NOT NULL
 );
 
@@ -9,9 +10,11 @@ CREATE TABLE IF NOT EXISTS posts (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     id_user INTEGER NOT NULL,
     text TEXT NOT NULL,
+	date TEXT NOT NULL,
 
     FOREIGN KEY (id_user) 
         REFERENCES users (id)
+        ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS friends (
@@ -20,29 +23,30 @@ CREATE TABLE IF NOT EXISTS friends (
 	date TEXT NOT NULL,
 
     FOREIGN KEY (id_user_first) 
-        REFERENCES users (id), 
+        REFERENCES users (id)
+        ON DELETE CASCADE, 
     FOREIGN KEY (id_user_second) 
         REFERENCES users (id)  
+        ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS requests (
     id_user_first INTEGER NOT NULL,
-	id_user_second INTEGER NOT NULL,
+	id_user_second INTEGER NOT NULL, 
 
     FOREIGN KEY (id_user_first) 
-        REFERENCES users (id),
+        REFERENCES users (id)
+        ON DELETE CASCADE,
     FOREIGN KEY (id_user_second) 
-        REFERENCES users (id)   
+        REFERENCES users (id)
+        ON DELETE CASCADE   
 );
 
 -- inserts
-INSERT INTO 
-    users (id, username, passwd) 
+INSERT OR IGNORE INTO 
+    users (email, username, passwd) 
     VALUES 
-        (1, "admin", "123"),
-        (2, "user1", "pass1"),
-        (3, "user2", "pass2"),
-        (4, "user3", "pass3")
-    ON CONFLICT (id) DO UPDATE SET
-        username = excluded.username,
-        passwd = excluded.passwd;
+        ("admin@gmail.com", "admin", "123"),
+        ("user1@gmail.com", "user1", "pass1"),
+        ("user2@gmail.com", "user2", "pass2"),
+        ("user3@gmail.com", "user3", "pass3");
