8cf8cf577f98f30df81fab5d7655f90b3182b7d7
Sandra Juarez <sandjuarezg@gmail.com>
2021-12-27 20:53:14 -0600

    refactore: improve structure

--- models/user.go
+++ models/user.go
@@ -1,26 +1,27 @@
 package models
 
 import (
+	"database/sql"
 	"errors"
 	"fmt"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
-// User: structure for User
+// User structure for User
 type User struct {
-	Id       int    // id of user
+	ID       int    // id of user
 	Email    string // email of user
 	Username string // username of user
-	Passwd   string // password of user
+	Password string // password of user
 }
 
-// AddUser: add user of the "users" table
-//  @param1 (u User): structure variable "User"
+// AddUser add user of the "users" table
+//  @param1 (user): structure variable "User"
 
 //  @return1 (err error): error variable
-func AddUser(u User) (err error) {
-	_, err = DB.Exec("INSERT INTO users (email, username, passwd) VALUES (?, ?, ?)", u.Email, u.Username, u.Passwd)
+func AddUser(user User) (err error) {
+	_, err = DB.Exec("INSERT INTO users (email, username, passwd) VALUES (?, ?, ?)", user.Email, user.Username, user.Password)
 	if err != nil {
 		err = errors.New("error: probably email already use")
 		return
@@ -29,29 +30,31 @@ func AddUser(u User) (err error) {
 	return
 }
 
-// LogIn: user login
-//  @param1 (email  string): email of user
-//  @param2 (passwd string): password of user
+// LogIn user login
+//  @param1 (email): email of user
+//  @param2 (passwd): password of user
 //
-//  @return1 (u User): structure variable "User"
+//  @return1 (user User): structure variable "User"
 //  @return2 (err error): error variable
-func LogIn(email, passwd string) (u User, err error) {
+func LogIn(email, passwd string) (user User, err error) {
 	row := DB.QueryRow("SELECT id, email, username, passwd FROM users WHERE email = ? AND passwd = ?", email, passwd)
-	err = row.Scan(&u.Id, &u.Email, &u.Username, &u.Passwd)
+	err = row.Scan(&user.ID, &user.Email, &user.Username, &user.Password)
 	if err != nil {
-		err = errors.New("User not found")
+		if err == sql.ErrNoRows {
+			err = errors.New("User not found")
+		}
 		return
 	}
 
 	return
 }
 
-// DeleteAccount: delete account of user
-//  @rcvr1 (u User): structure variable "User"
+// DeleteAccount delete account of user
+//  @rcvr1 (user): structure variable "User"
 //
 //  @return1 (err error): error variable
-func (u User) DeleteAccount() (err error) {
-	row, err := DB.Exec("DELETE from users WHERE id = ?", u.Id)
+func (user User) DeleteAccount() (err error) {
+	row, err := DB.Exec("DELETE from users WHERE id = ?", user.ID)
 	if err != nil {
 		return
 	}
@@ -69,12 +72,12 @@ func (u User) DeleteAccount() (err error) {
 	return
 }
 
-// GetSimilarUsersByUsername: get similar usernames by username
-//  @param1 (username string): username
+// GetSimilarUsersByUsername get similar usernames by username
+//  @param1 (username): username
 //
-//  @return1 (u []User): user slice
+//  @return1 (users []User): user slice
 //  @return2 (err error): error variable
-func GetSimilarUsersByUsername(username string) (u []User, err error) {
+func GetSimilarUsersByUsername(username string) (users []User, err error) {
 	rows, err := DB.Query("SELECT id, username FROM users WHERE username LIKE ? ORDER BY id", fmt.Sprintf("%%%s%%", username))
 	if err != nil {
 		return
@@ -84,27 +87,29 @@ func GetSimilarUsersByUsername(username string) (u []User, err error) {
 	var aux User
 
 	for rows.Next() {
-		err = rows.Scan(&aux.Id, &aux.Username)
+		err = rows.Scan(&aux.ID, &aux.Username)
 		if err != nil {
 			return
 		}
 
-		u = append(u, aux)
+		users = append(users, aux)
 	}
 
 	return
 }
 
-// GetUsernameByUserId: get username by id
-//  @param1 (id int): user id
+// GetUsernameByUserID get username by id
+//  @param1 (id): user id
 //
 //  @return1 (username string): username
 //  @return2 (err error): error variable
-func GetUsernameByUserId(id int) (username string, err error) {
+func GetUsernameByUserID(id int) (username string, err error) {
 	row := DB.QueryRow("SELECT username FROM users WHERE id = ?", id)
 	err = row.Scan(&username)
 	if err != nil {
-		err = errors.New("User not found")
+		if err == sql.ErrNoRows {
+			err = errors.New("User not found")
+		}
 		return
 	}
 
