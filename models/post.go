262c49df0283625ffdc7c01af5b8761c41a246ab
Sandra Juarez <sandjuarezg@gmail.com>
2022-01-17 18:30:02 -0600

    refactor: add null type

--- models/post.go
+++ models/post.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"database/sql"
 	"time"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -12,6 +13,14 @@ type Post struct {
 	IDUser int       // id of user
 	Text   string    // text of post
 	Date   time.Time // date of post
+}
+
+// Aux post structure for posts
+type AuxPost struct {
+	ID     sql.NullInt64  // id of post
+	IDUser sql.NullInt64  // id of user
+	Text   sql.NullString // text of post
+	Date   sql.NullTime   // date of post
 }
 
 // AddPost add post of the "posts" table
@@ -33,26 +42,40 @@ func AddPost(post Post) (err error) {
 //  @return1 (posts): post slice
 //  @return2 (err): error variable
 func GetPostsByUserID(id int) (posts []Post, err error) {
+	var (
+		auxPost AuxPost
+		aux     Post
+		date    sql.NullString
+	)
+
 	rows, err := DB.Query("SELECT date, text FROM posts WHERE user_id = ? ORDER BY date DESC", id)
 	if err != nil {
 		return
 	}
 	defer rows.Close()
 
-	var (
-		aux     Post
-		content string
-	)
-
 	for rows.Next() {
-		err = rows.Scan(&content, &aux.Text)
+		err = rows.Scan(&date, &auxPost.Text)
 		if err != nil {
 			return
 		}
 
-		aux.Date, err = time.Parse(time.RFC3339, string(content))
+		aux.Date, err = time.Parse(time.RFC3339, "0000-01-01T00:00:00Z")
 		if err != nil {
 			return
+		}
+
+		if date.Valid {
+			aux.Date, err = time.Parse(time.RFC3339, date.String)
+			if err != nil {
+				return
+			}
+		}
+
+		aux.Text = "NULL"
+
+		if auxPost.Text.Valid {
+			aux.Text = auxPost.Text.String
 		}
 
 		posts = append(posts, aux)
